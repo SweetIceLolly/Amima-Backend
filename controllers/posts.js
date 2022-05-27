64deae0bbade45313e2c43464f1aa9032b86fc07
John the second <wu103@mcmaster.ca>
2022-05-27 16:45:30 -0400

    cleaned up back end

--- controllers/posts.js
+++ controllers/posts.js
@@ -170,7 +170,7 @@ function edit_post(req, res, next) {
     return utils.response(req, res, 400, {error: 'Missing required fields'});
   }
 
-  User.findOneAndUpdate({ _id : req.body.auth_user }, { "$set": {
+  User.findOneAndUpdate({ _id : req.body.auth_user_id }, { "$set": {
     title: req.body.title, 
     body: req.body.body, 
     content: req.body.content,
